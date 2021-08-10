ec9f687b853817db80b5797d873dba1646a278f9
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-08-10 11:13:50 +0530

    Remade the Project from IntelliJ Idea

--- 0_introduction/src/com/abhiroop/Main.java
+++ 0_introduction/src/com/abhiroop/Main.java
@@ -1,21 +1,20 @@
 package com.abhiroop;
 
-import javax.swing.JFrame; //used for making a window for displaying draws
+import javax.swing.JFrame; // The Window that will be displayed
 
 public class Main {
 
     private static JFrame getDisplayWindow() {
         JFrame displayWindow = new JFrame();
         displayWindow.setVisible(true); // generates a window on operating system
-        displayWindow.setSize(1000, 1000); // set the window's default size
+        displayWindow.setSize(400, 400); // set the window's default size
         displayWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // makes sure to close program execution when
-                                                                      // window closed
+        // window closed
         return displayWindow;
     }
 
     public static void main(String[] args) {
         JFrame displayWindow = getDisplayWindow();
-        displayWindow.add(new DisplayGraphics()); // displays the graphic drawing
+        displayWindow.add(new Painter()); // displays the graphic drawing
     }
-
 }
---
+++ 0_introduction/src/com/abhiroop/Painter.java
@@ -0,0 +1,10 @@
+package com.abhiroop;
+
+import java.awt.*;
+
+public class Painter extends Canvas {
+    @Override
+    public void paint(Graphics g) {
+        setBackground(Color.RED);
+    }
+}
