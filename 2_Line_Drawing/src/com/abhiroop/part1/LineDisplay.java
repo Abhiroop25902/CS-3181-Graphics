5a09882655e9b92ee2651474dc0f47625ae31f4a
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-09-07 14:58:59 +0530

    added graph drawing

--- 2_Line_Drawing/src/com/abhiroop/part1/LineDisplay.java
+++ 2_Line_Drawing/src/com/abhiroop/part1/LineDisplay.java
@@ -7,6 +7,7 @@ public class LineDisplay extends Canvas {
     //center origin
     private final int xOrigin;
     private final int yOrigin;
+    private final JFrame displayWindow;
 
     public LineDisplay(JFrame displayWindow) {
         //set Background Color
@@ -16,6 +17,7 @@ public class LineDisplay extends Canvas {
         xOrigin = (displayWindow.getX() + displayWindow.getWidth()) / 2;
         yOrigin = (displayWindow.getY() + displayWindow.getHeight() / 2);
 
+        this.displayWindow = displayWindow;
     }
 
     private void drawLineCircle(Graphics g, int length, int xCenter, int yCenter, LineDrawer lineDrawer, String text){
@@ -26,6 +28,59 @@ public class LineDisplay extends Canvas {
         }
 
         g.drawString(text, xOrigin + xCenter - text.length()/2*10, yOrigin - yCenter + length + 30);
+    }
+
+    private void drawLine(Graphics graphics, Color color, int x1,int y1, int x2, int y2){
+        graphics.setColor(color);
+        graphics.drawLine(xOrigin  + x1, yOrigin - y1, xOrigin + x2, yOrigin - y2);
+    }
+
+    private void drawString(Graphics g, Color color,  String str, int x, int y) {
+        var offset = 4;
+        g.setColor(color);
+        g.drawString(str, xOrigin + x + offset, yOrigin + y + offset + g.getFont().getSize());
+    }
+
+    private void drawGraph(Graphics g){
+        //going twice the window size to ensure zoom out works
+        var axisColor = new Color(3,133,88);
+        var graphLineColor = Color.GRAY;
+        var scale = 100;
+        var lineDrawer = new Bresenham(xOrigin, yOrigin);
+
+        var leftLimit = -1* (displayWindow.getWidth() - xOrigin) * 2;
+        var rightLimit = (displayWindow.getWidth() - xOrigin)*2;
+        var topLimit = (displayWindow.getHeight() - yOrigin) * 2;
+        var bottomLimit = -1* (displayWindow.getHeight() - yOrigin) * 2;
+
+
+        //x-axis
+        drawLine(g, axisColor,leftLimit, 0, rightLimit, 0);
+        //y-axis
+        drawLine(g, axisColor, 0,topLimit, 0, bottomLimit);
+
+        //origin
+        drawString(g,axisColor,"0", 0, 0);
+
+        //drawing Horizontal lines
+        for (int y= scale; y <= topLimit;y+= scale) {
+//            drawLine(g, graphLineColor, leftLimit, y, rightLimit, y);
+            drawString(g, axisColor,Integer.toString(-y), 0, y);
+        }
+        for (int y= -scale  ; y >= bottomLimit;y-= scale ) {
+//            drawLine(g, graphLineColor, leftLimit, y, rightLimit, y);
+            drawString(g, axisColor,Integer.toString(-y), 0, y);
+        }
+
+        //drawing vertical lines
+        for (int x= scale; x <= rightLimit;x+= scale ) {
+//            drawLine(g, graphLineColor, x, topLimit, x, bottomLimit);
+            drawString(g, axisColor, Integer.toString(x), x, 0);
+        }
+        for (int x= -scale; x >= leftLimit;x-= scale ) {
+//            drawLine(g, graphLineColor, x, topLimit, x, bottomLimit);
+            drawString(g, axisColor, Integer.toString(x), x, 0);
+        }
     }
 
     /**
@@ -44,6 +99,8 @@ public class LineDisplay extends Canvas {
      */
     @Override
     public void paint(Graphics g) {
+        drawGraph(g);
+        g.setColor(Color.BLACK);
         drawLineCircle(g, 100, -200, 200, new DDA(xOrigin, yOrigin), "DDA");
         drawLineCircle(g, 100, 200, 200, new Bresenham(xOrigin, yOrigin), "Bresenham");
         drawLineCircle(g, 100, 0, -200, new MidPoint(xOrigin, yOrigin), "Mid Point");
