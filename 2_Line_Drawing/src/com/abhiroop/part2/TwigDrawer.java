3b080b04a4a47e311d114a7837843c0b23fbeb5e
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-09-14 15:37:15 +0530

    added graph drawing

--- 2_Line_Drawing/src/com/abhiroop/part2/TwigDrawer.java
+++ 2_Line_Drawing/src/com/abhiroop/part2/TwigDrawer.java
@@ -10,6 +10,7 @@ public class TwigDrawer extends Canvas {
     private final int xOrigin;
     private final int yOrigin;
     private LineDrawer lineDrawer;
+    private final JFrame displayWindow;
 
     public TwigDrawer(JFrame displayWindow) {
         //set Background Color
@@ -18,6 +19,8 @@ public class TwigDrawer extends Canvas {
         //get origin of the displayWindow
         xOrigin = (displayWindow.getX() + displayWindow.getWidth()) / 2;
         yOrigin = (displayWindow.getY() + displayWindow.getHeight()) / 2;
+
+        this.displayWindow = displayWindow;
 
     }
 
@@ -35,12 +38,62 @@ public class TwigDrawer extends Canvas {
         }
     }
 
+    private void drawString(Graphics g, Color color,  String str, int x, int y) {
+        var offset = 4;
+        g.setColor(color);
+        g.drawString(str, xOrigin + x + offset, yOrigin + y + offset + g.getFont().getSize());
+    }
+
+    private void drawGraph(Graphics g){
+        //going twice the window size to ensure zoom out works
+        var axisColor = new Color(3,133,88);
+        var graphLineColor = Color.GRAY;
+        var scale = 100;
+        var lineDrawer = new Bresenham(xOrigin, yOrigin);
+
+        var leftLimit = -1* (displayWindow.getWidth() - xOrigin) * 2;
+        var rightLimit = (displayWindow.getWidth() - xOrigin)*2;
+        var topLimit = (displayWindow.getHeight() - yOrigin) * 2;
+        var bottomLimit = -1* (displayWindow.getHeight() - yOrigin) * 2;
+
+        g.setColor(axisColor);
+        //x-axis
+        drawLine(g, leftLimit, 0, rightLimit, 0, lineDrawer);
+        //y-axis
+        drawLine(g,  0,topLimit, 0, bottomLimit, lineDrawer);
+
+        //origin
+        drawString(g,axisColor,"0", 0, 0);
+
+        //drawing Horizontal lines
+        for (int y= scale; y <= topLimit;y+= scale) {
+//            drawLine(g, graphLineColor, leftLimit, y, rightLimit, y);
+            drawString(g, axisColor,Integer.toString(-y), 0, y);
+        }
+        for (int y= -scale  ; y >= bottomLimit;y-= scale ) {
+//            drawLine(g, graphLineColor, leftLimit, y, rightLimit, y);
+            drawString(g, axisColor,Integer.toString(-y), 0, y);
+        }
+
+        //drawing vertical lines
+        for (int x= scale; x <= rightLimit;x+= scale ) {
+//            drawLine(g, graphLineColor, x, topLimit, x, bottomLimit);
+            drawString(g, axisColor, Integer.toString(x), x, 0);
+        }
+        for (int x= -scale; x >= leftLimit;x-= scale ) {
+//            drawLine(g, graphLineColor, x, topLimit, x, bottomLimit);
+            drawString(g, axisColor, Integer.toString(x), x, 0);
+        }
+    }
+
     @Override
     public void paint(Graphics g) {
         int length = 100;
         int drawDirection = 90;
         lineDrawer = new Bresenham(xOrigin, yOrigin);
 
+        drawGraph(g);
+        g.setColor(Color.black);
         drawTwig(g, -300, 300, 1, length, drawDirection);
         drawTwig(g, -100, 300, 2, length, drawDirection);
         drawTwig(g, 100, 300, 3, length, drawDirection);
