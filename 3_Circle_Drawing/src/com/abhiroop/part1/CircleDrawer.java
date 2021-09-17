e82e19e08143bdad7135c956c2a8fe8764e49ae1
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-09-17 12:11:09 +0530

    Simplifies Mid-Point Circle Drawing Algorithm

--- 3_Circle_Drawing/src/com/abhiroop/part1/CircleDrawer.java
+++ 3_Circle_Drawing/src/com/abhiroop/part1/CircleDrawer.java
@@ -15,43 +15,35 @@ public class CircleDrawer {
     }
 
     public void draw(Graphics g, int xCenter, int yCenter, int radius){
-        int x = radius;
-        int  y= 0;
+        int x = 0;
+        int y = radius;
 
-        drawPoint(g, x+xCenter, y+yCenter);
+        int p = 1-radius;
 
-        if(radius > 0){
-            drawPoint(g, x+xCenter, -y + yCenter);
-            drawPoint(g, y+xCenter, x+yCenter);
-            drawPoint(g, -y + xCenter, x+yCenter);
-        }
-
-        int P = 1-radius;
-
-        while(x > y){
-            y++;
-            if(P<= 0)
-                P = P + 2*y + 1;
-            else{
-                x--;
-                P = P + 2*y - 2*x +1;
-            }
-
-            if(x<y)
-                break;
-
+        while(x<= y){
+            //1st quadrant
             drawPoint(g, x+xCenter, y+yCenter);
-            drawPoint(g, -x + xCenter, y + yCenter);
+            drawPoint(g, y+xCenter, x+yCenter);
+
+            //2nd quadrant
+            drawPoint(g, -x+xCenter, y+yCenter);
+            drawPoint(g, -y+xCenter, x+yCenter);
+
+            //3rd quadrant
             drawPoint(g, x+xCenter, -y+yCenter);
-            drawPoint(g, -x+xCenter, -y + yCenter);
+            drawPoint(g, y+xCenter, -x+yCenter);
 
-            if(x!= y){
-                drawPoint(g, y+xCenter, x+yCenter);
-                drawPoint(g, -y + xCenter, x + yCenter);
-                drawPoint(g, y+xCenter, -x+yCenter);
-                drawPoint(g, -y+xCenter, -x + yCenter);
+            //4th quadrant
+            drawPoint(g, -x+xCenter, -y+yCenter);
+            drawPoint(g, -y+xCenter, -x+yCenter);
+
+            if(p < 0)
+                p = p+ 2*x + 3;
+            else{
+                p = p + 2*(x-y) + 5;
+                y--;
             }
-
+            x++;
         }
     }
 }
