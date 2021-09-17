b09d6325e57561ae0b4a8d4ba0b5a1050c72e614
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-09-17 13:24:59 +0530

    Removed Fancy Circles for Performance

--- 3_Circle_Drawing/src/com/abhiroop/part1/CircleDisplay.java
+++ 3_Circle_Drawing/src/com/abhiroop/part1/CircleDisplay.java
@@ -14,7 +14,6 @@ public class CircleDisplay  extends BaseGraph {
     public void paint(Graphics g){
         super.paint(g);
 
-        for(int r = 0; r< 200; r+=2)
-            new CircleDrawer(super.getxOrigin(), super.getyOrigin()).draw(g, 0,0,r);
+        new CircleDrawer(super.getxOrigin(), super.getyOrigin()).draw(g, 0,0,100);
     }
 }
