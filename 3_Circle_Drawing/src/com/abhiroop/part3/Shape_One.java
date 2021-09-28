3d97417b17f26d6617f1f3cf5320e2d1537eed5a
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-09-28 16:26:20 +0530

    Edit: name change for easier typing

--- 3_Circle_Drawing/src/com/abhiroop/part3/Shape_One.java
+++ 3_Circle_Drawing/src/com/abhiroop/part3/Shape_One.java
@@ -10,25 +10,25 @@ public class Shape_One implements Shape{
     private final EllipseDrawer ellipseDrawer;
     private final CircleDrawer circleDrawer;
     private final Random rand;
-    private final int radX;
-    private final int radY;
+    private final int xRad;
+    private final int yRad;
     private final int numCircles;
     private final int radCircle;
 
     /**
      * Draws an ellipse and then multiple circles inside it
-     * @param ellipseDrawer the EllipseDrawer Object
-     * @param circleDrawer the CircleDrawer Object
-     * @param radX the x-axis radius length of the ellipse
-     * @param radY the y-axis radius length of the ellipse
+     * @param xOrigin xOrigin of the Frame
+     * @param yOrigin yOrigin of the Frame
+     * @param xRad the x-axis radius length of the ellipse
+     * @param yRad the y-axis radius length of the ellipse
      * @param numCircles number of circles to be made inside the ellipse
      * @param maxRadCircle maximum possible radius of the circles
      */
-    public Shape_One( EllipseDrawer ellipseDrawer, CircleDrawer circleDrawer, int radX, int radY, int numCircles, int maxRadCircle) {
-        this.ellipseDrawer = ellipseDrawer;
-        this.circleDrawer = circleDrawer;
-        this.radX = radX;
-        this.radY = radY;
+    public Shape_One(int xOrigin, int yOrigin, int xRad, int yRad, int numCircles, int maxRadCircle) {
+        this.ellipseDrawer = new EllipseDrawer(xOrigin, yOrigin);
+        this.circleDrawer = new CircleDrawer(xOrigin, yOrigin);
+        this.xRad = xRad;
+        this.yRad = yRad;
         this.numCircles = numCircles;
         this.radCircle = maxRadCircle;
         this.rand = new Random(System.currentTimeMillis());
@@ -46,9 +46,9 @@ public class Shape_One implements Shape{
      */
     @Override
     public void drawShape(Graphics g, int startX, int startY) {
-        ellipseDrawer.draw(g, startX,startY,radX,radY);
+        ellipseDrawer.draw(g, startX,startY, xRad, yRad);
 
         for(var i=0;i<numCircles;i++)
-            circleDrawer.draw(g, startX + rand(-(radX - 50), radX - 50),startY + rand(-(radY - 50), radY - 50), rand(5,radCircle));
+            circleDrawer.draw(g, startX + rand(-(xRad - 50), xRad - 50),startY + rand(-(yRad - 50), yRad - 50), rand(5,radCircle));
     }
 }
